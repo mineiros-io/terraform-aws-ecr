15c9f0e738fe6c47150d577ebb16ef8f6efcbad5
Marius Tolzmann <m@rius.berlin>
2020-03-07 14:03:37 +0100

    Remove template modules and exampels

--- modules/vpc-peering/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  description = "The ID of the VPC Peering Connection."
-  value       = aws_vpc_peering_connection.vpc_peering.id
-}
-
-output "accept_status" {
-  description = "The status of the VPC Peering Connection request."
-  value       = aws_vpc_peering_connection.vpc_peering.accept_status
-}
