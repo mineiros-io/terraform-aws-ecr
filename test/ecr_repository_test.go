6ef6dd0aafe02dc2c9b83baed1ea37653fa4c633
Soren Martius <soeren.martius@gmail.com>
2020-03-10 15:49:57 +0100

    add docker build to example

--- test/ecr_repository_test.go
+++ test/ecr_repository_test.go
@@ -1,18 +1,35 @@
 package test
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/gruntwork-io/terratest/modules/aws"
+
+	"github.com/gruntwork-io/terratest/modules/docker"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
 func TestECRRepository(t *testing.T) {
 	t.Parallel()
 
+	tag := "mineiros-io/test-image:v1"
+	text := "hello world"
+
+	dockerOptions := &docker.BuildOptions{
+		Tags:      []string{tag},
+		BuildArgs: []string{fmt.Sprintf("text=%s", text)},
+	}
+
+	docker.Build(t, "./", dockerOptions)
+
+	awsRegion := aws.GetRandomRegion(t, nil, nil)
 	terraformOptions := &terraform.Options{
-		// The path to where your Terraform code is located
 		TerraformDir: "../examples/ecr",
 		Upgrade:      true,
+		Vars: map[string]interface{}{
+			"aws_region": awsRegion,
+		},
 	}
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
