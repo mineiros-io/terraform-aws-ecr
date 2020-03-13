bd2265880dfca812f91e372ed2786c61729db19a
Soren Martius <soeren.martius@gmail.com>
2020-03-13 22:14:42 +0100

    format code properly

--- test/ecr_repository_test.go
+++ test/ecr_repository_test.go
@@ -18,7 +18,7 @@ import (
 	"github.com/docker/docker/client"
 
 	"github.com/gruntwork-io/terratest/modules/docker"
-	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	testStructure "github.com/gruntwork-io/terratest/modules/test-structure"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 
@@ -37,44 +37,46 @@ func TestECRRepository(t *testing.T) {
 
 	workingDir := "../examples/ecr"
 	repoName := strings.ToLower(fmt.Sprintf("ecr-repository-%s", random.UniqueId()))
-	test_structure.SaveString(t, workingDir, "repoName", repoName)
+	testStructure.SaveString(t, workingDir, "repoName", repoName)
 
 	// At the end of the test, destroy all created Terraform resources
-	defer test_structure.RunTestStage(t, "cleanup_terraform", func() {
+	defer testStructure.RunTestStage(t, "cleanup_terraform", func() {
 		undeployTerraform(t, workingDir)
 	})
 
 	// Deploy the example with Terraform
-	test_structure.RunTestStage(t, "deploy_terraform", func() {
+	testStructure.RunTestStage(t, "deploy_terraform", func() {
 		awsRegion := aws.GetRandomStableRegion(t, []string{"eu-west-1"}, nil)
-		test_structure.SaveString(t, workingDir, "awsRegion", awsRegion)
+		testStructure.SaveString(t, workingDir, "awsRegion", awsRegion)
 		deployUsingTerraform(t, repoName, awsRegion, workingDir)
 	})
 
 	// Authenticate with ECR and push the image
-	test_structure.RunTestStage(t, "docker_build_and_push", func() {
-		awsRegion := test_structure.LoadString(t, workingDir, "awsRegion")
+	testStructure.RunTestStage(t, "docker_build_and_push", func() {
+		awsRegion := testStructure.LoadString(t, workingDir, "awsRegion")
 
 		// Load AWS session
-		session, err := aws.NewAuthenticatedSession(awsRegion)
+		awsSession, err := aws.NewAuthenticatedSession(awsRegion)
 		if err != nil {
 			t.Fatalf("An error occurred while initializing the session %s", err)
 		}
 
-		authorizationDetails, err := getAuthorizationDetails(session, awsRegion)
+		authorizationDetails, err := getAuthorizationDetails(awsSession, awsRegion)
 		if err != nil {
 			t.Fatalf("An error occurred while trying to fetch the authorization details for ecr: %s", err)
 		}
 
 		// Get the valid docker repository name
 		repo := getValidECRRepositoryName(repoName, authorizationDetails)
+		version := "v1"
+		image := fmt.Sprintf("%s:%s", repo, version)
 
 		// New docker client
 		dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 
 		// At the end of this test, destroy all created Docker resources
-		defer test_structure.RunTestStage(t, "cleanup_docker", func() {
-			if _, err := undeployDocker(dockerClient, repo); err != nil {
+		defer testStructure.RunTestStage(t, "cleanup_docker", func() {
+			if _, err := undeployDocker(dockerClient, image); err != nil {
 				t.Fatalf("An error occurred while deleting the docker image: %s", err)
 			}
 		})
@@ -166,7 +168,7 @@ func deployUsingTerraform(t *testing.T, repoName string, awsRegion string, worki
 	}
 
 	// Save the Terraform Options struct
-	test_structure.SaveTerraformOptions(t, workingDir, terraformOptions)
+	testStructure.SaveTerraformOptions(t, workingDir, terraformOptions)
 
 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
@@ -174,9 +176,9 @@ func deployUsingTerraform(t *testing.T, repoName string, awsRegion string, worki
 
 // Undeploy the example using Terraform
 func undeployTerraform(t *testing.T, workingDir string) {
-	terraformOptions := test_structure.LoadTerraformOptions(t, workingDir)
+	terraformOptions := testStructure.LoadTerraformOptions(t, workingDir)
 	terraform.Destroy(t, terraformOptions)
-	test_structure.CleanupTestDataFolder(t, workingDir)
+	testStructure.CleanupTestDataFolder(t, workingDir)
 }
 
 // Undeploy docker
