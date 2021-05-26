134d58d57e14b0f6eb493f918d0af76b8bd32e7c
Soren Martius <soeren.martius@gmail.com>
2021-05-26 11:19:13 +0200

    feat: add support for terraform v0.15

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@
 # SET TERRAFORM AND PROVIDER REQUIREMENTS FOR RUNNING THIS MODULE
 # ---------------------------------------------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 0.12.20, < 0.15"
+  required_version = ">= 0.12.20, < 0.16"
 
   required_providers {
     aws = ">= 2.45, < 4.0"
